ca392e86c6778af442fcaf6917f2104d3bddea5b
NZRI (also NZ Laboratory)  by kaz-naz <61860354+NZRI-AZRI@users.noreply.github.com>
2020-07-25 17:37:38 +0900

    Add files via upload

--- otp.js
+++ otp.js
@@ -395,7 +395,7 @@ async function downloadBookMarkFile() {
     console.log('sign data(bookmark data) is ', signatureObject);
  
     //blob download
-    const blob = new Blob([signatureObject], {type : 'application/json'} );
+    var blob = new Blob([ content ], { "type" : "text/plain" });
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     document.body.appendChild(a);
@@ -404,7 +404,7 @@ async function downloadBookMarkFile() {
     a.click();
     a.remove();
     URL.revokeObjectURL(url);
-    
+
 }
 
 //公示栞読み込み部分
