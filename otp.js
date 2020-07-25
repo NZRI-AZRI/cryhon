eea4c9d7aabe0e509268556f70dd5da38c7e33f0
NZRI (also NZ Laboratory)  by kaz-naz <61860354+NZRI-AZRI@users.noreply.github.com>
2020-07-25 22:04:28 +0900

    Add files via upload

--- otp.js
+++ otp.js
@@ -457,9 +457,9 @@ form.myfile.addEventListener( 'change', function(e) {
         let uploadFile = JSON.parse(reader.result);
 
         //web3.jsによりサインを復号化する
-        let recoverSign = web3RecoverSignAddress(uploadFile);
+        let recoverSign = await web3RecoverSignAddress(uploadFile);
 
-        console.log( 'recover is ', recoverSign );  
+        console.log( 'recover is ', recoverSign);  
         /**
          * 復号化した後の戻り値がアカウントと同じならば秘密鍵とブックマークファイルは一致しているのでnftの持ち主であったことが分かる。
          * 持ち主の栞を持っているので10分のログイン権限を付与する。
